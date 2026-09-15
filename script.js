185917eb1c5894a35a86b5e2387890a9e4c6f13d
Ana Santos | Grupo Info <ana.santos@infostore-am.com.br>
2026-09-15 15:55:14 -0400

    Adiciona busca

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
+let todosProdutos = [];
+
 fetch("produtos.json")
-.then(response => response.json())
-.then(produtos => {
+  .then(response => response.json())
+  .then(produtos => {
 
-    let html = "";
+    todosProdutos = produtos;
 
-    produtos.forEach(produto => {
+    renderizarProdutos(produtos);
 
-        html += `
-            <div class="produto">
-                <h2>${produto.modelo}</h2>
-                <p>${produto.marca}</p>
-                <p>${produto.categoria}</p>
-            </div>
-        `;
+    document
+      .getElementById("busca")
+      .addEventListener("input", pesquisar);
 
-    });
+  });
 
-    document.getElementById("produtos").innerHTML = html;
+function renderizarProdutos(produtos) {
 
-});
+  let html = "";
+
+  produtos.forEach(produto => {
+
+    html += `
+      <div class="produto">
+
+        ${produto.imagem}
+
+        <h2>${produto.modelo}</h2>
+
+        <p>${produto.marca}</p>
+
+        <p>${produto.descricao}</p>
+
+        <p>${produto.categoria}</p>
+
+      </div>
+    `;
+
+  });
+
+  document.getElementById("produtos").innerHTML = html;
+
+}
+
+function pesquisar() {
+
+  const texto = document
+    .getElementById("busca")
+    .value
+    .toLowerCase();
+
+  const filtrados = todosProdutos.filter(produto => {
+
+    return (
+      produto.modelo.toLowerCase().includes(texto) ||
+      produto.marca.toLowerCase().includes(texto) ||
+      produto.categoria.toLowerCase().includes(texto)
+    );
+
+  });
+
+  renderizarProdutos(filtrados);
+
+}
